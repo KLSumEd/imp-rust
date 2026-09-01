61ddef87114b3db9cb6d6bb9dfedf597b6de45eb
Kai Sumner-Edwards <169290341+KLSumEd@users.noreply.github.com>
2026-09-01 19:21:47 +0100

    feat: switched from using derive to builder module

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,39 @@
+use clap::{Arg, ArgMatches};
 use std::error::Error;
 
-mod cli {
-    pub use clap::Parser;
-
-    /// A Command Line Interface struct deriving `clap::Parser`
-    #[derive(Parser, Debug)]
-    #[command(name = "imp")]
-    #[command(version, about, long_about = None)]
-    pub struct Cli {
-        #[arg()]
-        source_file: String,
-        #[arg()]
-        output_file: String,
-    }
-}
-
-use cli::{Cli, Parser};
+// mod cli {
+//     pub use clap::Parser;
+//
+//     /// A Command Line Interface struct deriving `clap::Parser`
+//     #[derive(Parser, Debug)]
+//     #[command(name = "imp")]
+//     #[command(version, about, long_about = None)]
+//     pub struct Cli {
+//         #[arg()]
+//         source_file: String,
+//         #[arg()]
+//         output_file: String,
+//     }
+// }
+//
+// use cli::{Cli, Parser};
 
 pub fn run() -> Result<(), Box<dyn Error>> {
-    let args: Cli = Cli::parse();
-    dbg!(args);
+    let cli_matches: ArgMatches = clap::command!()
+        .arg(Arg::new("source_file").required(true))
+        .arg(Arg::new("output_file").required(true))
+        .get_matches();
+
     Ok(())
 }
 
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn verify_cli() {
-        use clap::CommandFactory;
-        Cli::command().debug_assert();
-    }
-}
+// #[cfg(test)]
+// mod test {
+//     use super::*;
+//
+//     #[test]
+//     fn verify_cli() {
+//         use clap::CommandFactory;
+//         Cli::command().debug_assert();
+//     }
+// }
