4436bd66808815ebf16787a1f68606be119094ea
Kai <169290341+KLSumEd@users.noreply.github.com>
2026-09-23 18:51:56 +0100

    branch(this-error): refactoring to use this-error
    Switching to using the this-error library for custom error handling.

--- src/lib.rs
+++ src/lib.rs
@@ -1,99 +1,46 @@
 use std::error::Error;
 
 mod cli {
-    use std::path::PathBuf;
-
     // TODO: Implement CLI -- Kai
     //      1. Must be extensible
     //      2. Must be constant (i.e. processed at compile-time)
     //      3. Attempt to follow Open-Closed principle (i.e. should be able
     //          to add new commands/options without modifying existing structure)
+    //
+    //  Consider using imperative approach
+    //      - Match command
+    //      - command_name => handler_fn()
+    //      - fn handler_fn() { /* do custom arg parsing */ }
+    //      - For fundamental parsing, use shared parse_arg() function(s)
 
-    /// A Command Line Interface struct deriving `clap::Parser`
-    #[derive(Debug)]
-    pub struct Cli {
-        command: &'static Command,
-    }
+    use std::env::{self, Args};
 
-    impl Cli {
-        const CMD_BUILD: &'static Command<1> = &Command {
-            name: "build",
-            args: [&Arg {
-                name: "source_file(s)",
-                arg_type: &ArgType::PATHS,
-            }],
+    pub fn parse() {
+        let mut args: Args = env::args();
+
+        let Some(command) = args.next() else {
+            CliError::handle_error(CliError::NoCmd)
         };
-        const CMD_TEST: &'static Command<1> = &Command {
-            name: "test",
-            args: [&Arg {
-                name: "test_file(s)",
-                arg_type: &ArgType::PATHS,
-            }],
-        };
-        const CMD_LINT: &'static Command<1> = todo!();
-        const CMD_FORMAT: &'static Command<1> = todo!();
+
+        unimplemented!()
     }
 
-    impl Cli {
-        pub fn parse() -> Self {
-            unimplemented!();
-        }
-    }
+    fn parse_command() {}
 
-    #[derive(Debug)]
-    struct Command<const N: usize> {
+    struct CliError {
+        code: u16,
         name: &'static str,
-        args: [&'static Arg; N],
+        description: &'static str,
     }
 
-    #[derive(Debug)]
-    struct Arg {
-        name: &'static str,
-        arg_type: &'static ArgType,
-    }
-
-    #[derive(Debug)]
-    enum ArgType {
-        PATH,
-        BOOL,
-        PATHS,
-        STRINGS,
-    }
-
-    #[derive(Debug)]
-    enum Opt<T> {
-        Short {
-            id: &'static str,
-            desc: &'static str,
-            value: T,
-        },
-        Long {
-            id: &'static str,
-            desc: &'static str,
-            value: T,
-        },
-        Full {
-            short_id: &'static str,
-            desc: &'static str,
-            value: T,
-        },
-    }
-
-    impl<T> Opt<T> {
-        pub fn get_value(self: Self) -> T {
-            match self {
-                Opt::Short { value: val, .. } => val,
-                Opt::Long { value: val, .. } => val,
-                Opt::Full { value: val, .. } => val,
-            }
+    impl CliError {
+        fn handle_error(error: CliError) -> ! {
+            todo!()
         }
     }
 }
 
-use cli::Cli;
-
 pub fn run() -> Result<(), Box<dyn Error>> {
-    let args: Cli = Cli::parse();
-    dbg!(args);
+    cli::parse();
     Ok(())
 }
